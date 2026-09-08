54cb6e002e8011530031001854440854e89c1d4e
Patricio Valdes <valdespatricio832@gmail.com>
2026-09-08 18:35:38 -0300

    fix(auth): evitar crash cuando un rol valido no tiene tabs asignados

--- mobile/src/navigation/permissions.js
+++ mobile/src/navigation/permissions.js
@@ -9,7 +9,7 @@ const TABS_POR_ROL = {
   [ROLES.REPORTANTE]: ['Inicio', 'Escanear', 'Perfil'],
   [ROLES.SUPERVISOR]: ['Inicio', 'Escanear', 'Perfil'],
   [ROLES.TECNICO]: ['Inicio', 'Escanear', 'Perfil'],
-  // [ROLES.ADMINISTRADOR]: ['Inicio', 'Perfil'],
+  [ROLES.ADMINISTRADOR]: ['Inicio', 'Perfil'],
 };
 
 function sinAcentos(texto) {
